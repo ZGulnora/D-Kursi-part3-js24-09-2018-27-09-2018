633396d0204b88a60259f4485a0e6bc27fe1f441
guli <gulisha64@mail.ru>
2018-10-10 21:08:33 +0500

    Домашка по 4 уроку

---
+++ js/script.js
@@ -0,0 +1,115 @@
+//Домашка к уроку 4 js
+//variant 1
+
+var age = prompt ('Cколько Вам лет?', '');
+// if (age>=14) {
+// 	alert('входите в рамки ');
+// }else if (age<=90) {
+// 	alert('входите в рамки ');
+// }else {
+// 	alert('не входите в рамки ');
+// }
+
+// variant 2
+
+// if (age<=14) {
+// 	alert('входите в рамки ');
+// }else if (age>=90) {
+// 	alert('входите в рамки ');
+// }else {
+// 	alert('не входите в рамки ');
+// }
+
+if (age!=14);
+if (age<=13) {
+	alert('входите в рамки ');
+} else if (age>=90) {
+	alert('входите в рамки ');
+}else {
+	alert('не входите в рамки ');
+}
+
+
+// function prov(){ 
+// 	var a=document.forma1.elements[2].value; 
+// 	var b=document.forma1.elements[3].value; 
+// 	if (a==b){ 
+// 	alert ("Вы зарегистрированы!"); } 
+// 	else { 
+// 	alert ("Введите правильный пароль."); } }
+
+
+
+
+
+
+
+
+// var browser = prompt("Название браузера?");
+// if ( browser == 'IE'){
+// 	alert('О да у вас IE');
+// }else
+// if (browser == 'Chrome' || browser == 'Safari' || browser == 'Opera'){
+
+// 	alert ('Дa эти браузеры мы поддерживает');
+// } else{
+// 	alert ('Мы надеемся, что и в вашем браузере все Ок.')
+// }
+
+
+
+// var a = +prompt ('a?', 100)
+// switch (a)
+// {
+// 	case (0):alert(0);break;
+// 	case (1):alert (1);break;
+// 	case (2):
+// 	case (3):alert( 2.3);break;
+// 	default:
+// 	alert('ups');
+// }
+
+
+// var a=5, b=10;
+// // 1 вариант , проверяет условие если Верно
+// if (a==b){
+// alert('Да равно!')
+// }
+// // 2 вариант
+// if(a==b){
+// alert('Да равно!')	
+// }else {
+// alert('Heт не равно!')	
+// }
+// // 3 вариант
+// if(a<b){
+// alert('А - меньше ')	
+// }else if(a==b){
+// alert('А равно В ')	
+// } else{
+// alert('В - меньше ')
+// }
+
+// function bigPict() {
+// 	var w=document.princ.width;
+// 	if (w<300){
+// 		document.princ.width=w+10;
+// 		document.princ.ыкс="princ.jpg";
+// 		setTimeout ("bigPict()", 500)
+// 	}
+// }
+
+
+
+// function maxZ(asd){
+// 	var a=1*asd.zn1.value;
+// 	var b=1*asd.zn2.value;
+// 	var c=1*asd.zn3.value;
+// 	var m=a;
+// 	if (b>m){ m=b;}
+// 	if (c>m) { m=c;}
+// 	asd.rez.value=m;
+// }
+
+//document.body.style.backgroundColor = "red";
+	
